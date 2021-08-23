ed11e99160edaf58f55ec4739ab107279603341b
mcafd@amazon.com <mcafd@amazon.com>
2021-08-23 14:27:16 -0700

    add apolloV3 Provider

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import Amplify, { Auth } from "aws-amplify";
 import config from "./src/aws-exports";
+import { gql, useSubscription } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { withAuthenticator } from "aws-amplify-react-native";
 
 Amplify.configure(config);
@@ -31,29 +33,147 @@ const App = () => {
       .catch((err) => console.log(err));
   }
 
-  async function fetchTodos() {
-    try {
-      const todoData = await API.graphql(graphqlOperation(listTodos));
-      const todos = todoData.data.listTodos.items;
-      setTodos(todos);
-    } catch (err) {
-      console.log("error fetching todos");
+  // async function fetchTodos() {
+  //   try {
+  //     const todoData = await API.graphql(graphqlOperation(listTodos));
+  //     const todos = todoData.data.listTodos.items;
+  //     setTodos(todos);
+  //   } catch (err) {
+  //     console.log("error fetching todos");
+  //   }
+  // }
+
+  // async function addTodo() {
+  //   try {
+  //     const todo = { ...formState };
+  //     setTodos([...todos, todo]);
+  //     setFormState(initialState);
+  //     await API.graphql(graphqlOperation(createTodo, { input: todo }));
+  //   } catch (err) {
+  //     console.log("error creating todo:", err);
+  //   }
+  // }
+
+  // List
+  const LIST_TODOS = gql`
+    query listTodos {
+      listTodos {
+        items {
+          id
+          name
+          description
+        }
+      }
     }
+  `;
+
+  const {
+    loading: listLoading,
+    data: listData,
+    error: listError,
+  } = useQuery(LIST_TODOS);
+
+  useEffect(() => {
+    if (listData) {
+      setTodos(listData?.listTodos?.items);
+    }
+  }, [listData]);
+
+  // Create
+  const CREATE_TODO = gql`
+    mutation createTodo($input: CreateTodoInput!) {
+      createTodo(input: $input) {
+        id
+        name
+        description
+      }
+    }
+  `;
+
+  // Optional: use `data`, `loading`, and `error`.
+  // Can also use refretch if not using a subscription:
+  // const [addTodoMutateFunction] = useMutation(CREATE_TODO, {
+  //   refetchQueries: [LIST_TODOS, "listTodos"],
+  // });
+  const [addTodoMutateFunction, { error: createError }] =
+    useMutation(CREATE_TODO);
+
+  if (createError) {
+    console.error(createError);
   }
 
   async function addTodo() {
     try {
-      const todo = { ...formState };
-      setTodos([...todos, todo]);
+      if (!formState.name || !formState.description) return;
+      const todoId = uuidv4();
+      const todo = { id: todoId, ...formState };
       setFormState(initialState);
-      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+      addTodoMutateFunction({ variables: { input: { ...todo } } });
     } catch (err) {
       console.log("error creating todo:", err);
     }
   }
 
+  // Delete
+  // const DELETE_TODO = gql`
+  //   mutation deleteTodo($input: DeleteTodoInput!) {
+  //     deleteTodo(input: $input) {
+  //       id
+  //       name
+  //       description
+  //     }
+  //   }
+  // `;
+
+  // Optional: use `data`, `loading`, and `error`
+  // const [deleteTodoMutateFunction] = useMutation(DELETE_TODO, {
+  //   refetchQueries: [LIST_TODOS, "listTodos"],
+  // });
+
+  // async function removeTodo(id) {
+  //   try {
+  //     deleteTodoMutateFunction({ variables: { input: { id } } });
+  //   } catch (err) {
+  //     console.log("error deleting todo:", err);
+  //   }
+  // }
+
+  // Subscriptions
+  const CREATE_TODO_SUBSCRIPTION = gql`
+    subscription OnCreateTodo {
+      onCreateTodo {
+        id
+        name
+        description
+      }
+    }
+  `;
+
+  const { data: createSubData, error: createSubError } = useSubscription(
+    CREATE_TODO_SUBSCRIPTION
+  );
+
+  if (
+    createSubData &&
+    todos.filter((todo) => todo.id === createSubData?.onCreateTodo?.id)
+      .length === 0
+  ) {
+    setTodos([...todos, createSubData?.onCreateTodo]);
+  } else if (createSubError) {
+    console.error(createSubError);
+  }
+
+  let status = "";
+
+  if (listLoading) {
+    status = "Loading todos...";
+  } else if (listError) {
+    status = "Error loading todos!";
+  }
+
   return (
     <View style={styles.container}>
+      <Text>"Todos using Apollo V3"</Text>
       <Button title="Sign Out" onPress={signOut} />
       <TextInput
         onChangeText={(val) => setInput("name", val)}
