25118b022b1646e9a3dd7e0da900fb48b9c62677
mcafd@amazon.com <mcafd@amazon.com>
2021-08-23 17:49:02 -0700

    add AppSync V2 implementation, add subscription

--- App.js
+++ App.js
@@ -1,16 +1,55 @@
-import { Auth } from "aws-amplify";
-import { gql, useSubscription } from "@apollo/client";
-import { useMutation, useQuery } from "@apollo/client";
-import { v4 as uuidv4 } from "uuid";
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import gql from "graphql-tag";
+import { graphql, compose } from "react-apollo";
+import { graphqlMutation } from "aws-appsync-react";
+// TODO: add to react web demo
+import { buildSubscription } from "aws-appsync";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+// include description in docs to match default setup
+const CreateTodo = gql`
+  mutation createTodo($name: String!, $description: String!) {
+    createTodo(input: { name: $name, description: $description }) {
+      name
+      description
+    }
+  }
+`;
+
+// include description in docs to match default setup
+const listTodos = gql`
+  query listTodo {
+    listTodos {
+      items {
+        id
+        name
+        description
+      }
+    }
+  }
+`;
+
+// TODO: add to react web demo
+const PostSubscription = gql`
+  subscription postSubscription {
+    onCreatePost {
+      id
+      name
+    }
+  }
+`;
 
 const initialState = { name: "", description: "" };
 
 const App = () => {
   const [formState, setFormState] = useState(initialState);
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    this.props.data.subscribeToMore(
+      buildSubscription(PostSubscription, listPosts)
+    );
+  }, []);
 
   function setInput(key, value) {
     setFormState({ ...formState, [key]: value });
@@ -22,125 +61,11 @@ const App = () => {
       .catch((err) => console.log(err));
   }
 
-  // List
-  const LIST_TODOS = gql`
-    query listTodos {
-      listTodos {
-        items {
-          id
-          name
-          description
-        }
-      }
-    }
-  `;
-
-  const {
-    loading: listLoading,
-    data: listData,
-    error: listError,
-  } = useQuery(LIST_TODOS);
-
-  useEffect(() => {
-    if (listData) {
-      setTodos(listData?.listTodos?.items);
-    }
-  }, [listData]);
-
-  // Create
-  const CREATE_TODO = gql`
-    mutation createTodo($input: CreateTodoInput!) {
-      createTodo(input: $input) {
-        id
-        name
-        description
-        createdAt
-        updatedAt
-      }
-    }
-  `;
-
-  // Optional: use `data`, `loading`, and `error`.
-  // Can also use refretch if not using a subscription:
-  // const [addTodoMutateFunction] = useMutation(CREATE_TODO, {
-  //   refetchQueries: [LIST_TODOS, "listTodos"],
-  // });
-  const [addTodoMutateFunction, { error: createError }] =
-    useMutation(CREATE_TODO);
-
-  if (createError) {
-    console.error(createError);
-  }
-
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.description) return;
-      const todoId = uuidv4();
-      const todo = { id: todoId, ...formState };
-      setFormState(initialState);
-      addTodoMutateFunction({ variables: { input: { ...todo } } });
-    } catch (err) {
-      console.log("error creating todo:", err);
-    }
-  }
-
-  // Delete
-  const DELETE_TODO = gql`
-    mutation deleteTodo($input: DeleteTodoInput!) {
-      deleteTodo(input: $input) {
-        id
-        name
-        description
-      }
-    }
-  `;
-
-  // Optional: use `data`, `loading`, and `error`
-  const [deleteTodoMutateFunction] = useMutation(DELETE_TODO, {
-    refetchQueries: [LIST_TODOS, "listTodos"],
-  });
-
-  async function removeTodo(id) {
-    try {
-      deleteTodoMutateFunction({ variables: { input: { id } } });
-    } catch (err) {
-      console.log("error deleting todo:", err);
-    }
-  }
-
-  const CREATE_TODO_SUBSCRIPTION = gql`
-    subscription OnCreateTodo {
-      onCreateTodo {
-        id
-        name
-        description
-        createdAt
-        updatedAt
-      }
-    }
-  `;
-
-  const { data: createSubData, error: createSubError } = useSubscription(
-    CREATE_TODO_SUBSCRIPTION
-  );
-
-  if (
-    createSubData &&
-    todos.filter((todo) => todo.id === createSubData?.onCreateTodo?.id)
-      .length === 0
-  ) {
-    setTodos([...todos, createSubData?.onCreateTodo]);
-  } else if (createSubError) {
-    console.error(createSubError);
-  }
-
-  let status = "";
-
-  if (listLoading) {
-    status = "Loading todos...";
-  } else if (listError) {
-    status = "Error loading todos!";
-  }
+  addTodo = () =>
+    this.props.createTodo({
+      name: this.state.name,
+      description: this.state.description,
+    });
 
   return (
     <View style={styles.container}>
@@ -178,4 +103,14 @@ const styles = StyleSheet.create({
   todoName: { fontSize: 18 },
 });
 
-export default App;
+export default compose(
+  graphql(listTodos, {
+    options: {
+      fetchPolicy: "cache-and-network",
+    },
+    props: (props) => ({
+      todos: props.data.listTodos ? props.data.listTodos.items : [],
+    }),
+  }),
+  graphqlMutation(CreateTodo, listTodos, "Todo")
+)(App);
